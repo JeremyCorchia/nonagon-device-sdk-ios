39f6854d3c5792fcaafe4e9d720905c7c54c216d
Jeremy Corchia <jeremy.corchia@gmail.com>
2026-04-14 18:41:45 +0300

    fix: 1.1.4 — CAlgorithms module embedded in framework

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let version = "1.1.3"
+let version = "1.1.4"
 
 let package = Package(
     name: "NonagonDeviceSDK",
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "NonagonDeviceSDK",
             url: "https://github.com/JeremyCorchia/nonagon-device-sdk-ios/releases/download/\(version)/NonagonDeviceSDK.xcframework.zip",
-            checksum: "75b3969251f02bbe6078d14189e722db46cc944f7021f2d26f731a595e40c3a5"
+            checksum: "20640221466ddaad0e20ae08a441d4a3ec3453c91972818b808e9cb05af2129d"
         ),
     ]
 )

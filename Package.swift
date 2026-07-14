60bcb29f994cf51733a601c514601555a94cd613
Jeremy Corchia <jeremy.corchia@gmail.com>
2026-07-14 12:59:57 +0300

    fix: 2.0.1 — honor OximeterConfig stopOnValidResult/qualityThreshold/loopMode

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let version = "2.0.0"
+let version = "2.0.1"
 
 let package = Package(
     name: "NonagonDeviceSDK",
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "NonagonDeviceSDK",
             url: "https://github.com/JeremyCorchia/nonagon-device-sdk-ios/releases/download/\(version)/NonagonDeviceSDK.xcframework.zip",
-            checksum: "5e2ed7840dafd26017ff1107df2734c666ea08ba222bafef1318405c934ad3cd"
+            checksum: "cea4b7679f02fb6009550dce34b5429261128a7c447404b565a3576e0e8074e5"
         ),
     ]
 )

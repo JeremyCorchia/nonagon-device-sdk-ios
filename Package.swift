0c4e19f17409ef59377058fff26c69ee4bdd3dc9
Jeremy Corchia <jeremy.corchia@gmail.com>
2026-07-14 12:46:00 +0300

    chore: release 2.0.0 — Android-aligned API, engine internalized

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let version = "1.2.0"
+let version = "2.0.0"
 
 let package = Package(
     name: "NonagonDeviceSDK",
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "NonagonDeviceSDK",
             url: "https://github.com/JeremyCorchia/nonagon-device-sdk-ios/releases/download/\(version)/NonagonDeviceSDK.xcframework.zip",
-            checksum: "13c06cc89433482427219a9e6908bc8ec2a21f926b481d1793da169d120dfd77"
+            checksum: "5e2ed7840dafd26017ff1107df2734c666ea08ba222bafef1318405c934ad3cd"
         ),
     ]
 )
